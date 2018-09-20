2be36d0f5a43507060cfe6b1d19d98e27f83b3a0
WebHW <664322480@qq.com>
2018-09-21 07:09:28 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -5,9 +5,9 @@ module.exports = appInfo => {
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1519887194138_3450';
-
   // add your config here
   config.middleware = [];
+  config.mongodb = 'mongodb://127.0.0.1/node_club_test';
 
   return config;
 }
