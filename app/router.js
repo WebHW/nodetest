66543ba442b4404b146a06d41e2f9280f678c446
wang <664322480@qq.com>
2018-09-21 18:40:02 +0800

    a
    a

--- app/router.js
+++ app/router.js
@@ -10,5 +10,14 @@ module.exports = app => {
   router.get('/', middleware.authUser(), controller.site.index);
   return;
   // sitemap
-  // router.get('/sitemap.xml', controller.site.sitemap);
+  // sign controller
+  if (config.allow_sign_up) {
+    router.get('/signup', controller.sign.showSignup); // 跳转到注册页面
+    router.post('/signup', controller.sign.signup); // 提交注册信息
+  } else {
+    // 进行github验证
+    router.get('/signup', function(req, res, next) {
+      return res.redirect('/auth/github');
+    });
+  }
 };
